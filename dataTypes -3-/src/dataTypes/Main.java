90c5bd82a2159d899008fe11f5f1eecc5a004c74
Kadir Köroğlu <kadirkoroglu10@gmail.com>
2023-11-11 16:05:54 +0300

    dataTypes

---
+++ dataTypes -3-/src/dataTypes/Main.java
@@ -0,0 +1,21 @@
+package dataTypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		double sayi =12.5; 
+		
+		sayi =127;
+		
+		String sehir ="Ankara";
+		
+		char karakter = 'A';
+		
+		boolean dogruMu = false ;
+		
+		
+
+	}
+
+}
