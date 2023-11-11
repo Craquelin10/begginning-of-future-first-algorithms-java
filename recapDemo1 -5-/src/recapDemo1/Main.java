577dae34a420afa9e8dff65c307817ba20f1739a
Kadir Köroğlu <kadirkoroglu10@gmail.com>
2023-11-11 17:19:38 +0300

    recapDemo
    En büyük sayıyı bulmaca

---
+++ recapDemo1 -5-/src/recapDemo1/Main.java
@@ -0,0 +1,29 @@
+package recapDemo1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi1 = 12;
+		int sayi2 = 15;
+		int sayi3 = 9;
+		
+		int enB = sayi1;
+		if (enB<sayi2) {
+			enB = sayi2; 
+			
+		} if (enB<sayi3) {
+			enB = sayi3;
+		}
+		System.out.println("en büyük sayı "+ enB + " dir");
+		
+	
+		
+			
+			
+		
+				
+
+	}
+
+}
+
