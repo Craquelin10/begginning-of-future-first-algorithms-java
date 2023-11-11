4942c732441216c091943ba96323f757c4e014da
Kadir Köroğlu <kadirkoroglu10@gmail.com>
2023-11-11 18:45:38 +0300

    loopDemo3
    Do-While

---
+++ loopDemo3 -9-/src/loopDemo3/Main.java
@@ -0,0 +1,17 @@
+package loopDemo3;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int j=100;
+		do {
+			System.out.println(j);
+			j++;
+					
+		}while(j<=10);
+		System.out.println("Do-While Döngüsü Bitti");
+		
+
+	}
+
+}
