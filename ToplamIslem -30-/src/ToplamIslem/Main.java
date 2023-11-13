938e82b1e8ff6bc81bee12ee21ff42f31c5ff8e1
Kadir Köroğlu <kadirkoroglu10@gmail.com>
2023-11-13 12:43:47 +0300

    ZarAtmaOyunu

---
+++ ToplamIslem -30-/src/ToplamIslem/Main.java
@@ -0,0 +1,51 @@
+package ToplamIslem;
+import java.util.*;
+public class Main {
+	private enum Durum {Devam, Kazan, Kaybet};
+	public static void main(String[] args) {
+		
+		Random num = new Random();
+		int puan = 0;
+		Durum OyunDurumu;
+		int zarToplami = zarAt(num);
+		System.out.println("Toplam= "+zarToplami);
+		
+		switch(zarToplami) {
+		case 7:
+			case 11: OyunDurumu = Durum.Kazan;
+		break;
+		case 2:
+		case 3:
+		case 12: OyunDurumu = Durum.Kaybet;
+		break;
+		
+		default:
+			OyunDurumu = Durum.Devam;
+			puan = zarToplami;
+			System.out.println("Puan= "+puan);
+		}
+		while(OyunDurumu==Durum.Devam) {
+			zarToplami = zarAt(num);
+			System.out.println("Toplam= "+zarToplami);
+			if(zarToplami == puan)
+				OyunDurumu = Durum.Kazan;
+			else
+				if(zarToplami == 7)
+					OyunDurumu = Durum.Kaybet;		
+		}
+		if (OyunDurumu==Durum.Kazan)
+			System.out.println("KAZANDINIZ");
+		else 
+			System.out.println("KAYBETTİNİZ");
+	}
+	public static int zarAt(Random n) {
+		return (1+n.nextInt(6))+(1+n.nextInt(6));
+	}
+	
+	
+		
+		
+	}
+	
+	
+	
